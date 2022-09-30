5af0963468e26e57f3c1b6846f8fab0db966437c
Vivek Bhawsar <vivekcoder18@gmail.com>
2022-09-30 17:03:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	router := mux.NewRouter()
 	router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("AT-Tool_certificate-builder").HTTPBox()))
 	println("Listning at port : 12345") 
-	http.ListenAndServe(":5500" , router)
+	http.ListenAndServe(":3000" , router)
 }
