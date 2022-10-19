c82f06f788c05e55c22ab4e39e7664826247c927
Vivek Bhawsar <vivekcoder18@gmail.com>
2022-10-19 10:50:35 +0530

    File upload

--- AT-Tool_certificate-builder/script/script.js
+++ AT-Tool_certificate-builder/script/script.js
@@ -2,22 +2,22 @@
 // import { jsPDF } from 'jspdf';
 
 
-window.onload = function () {
-    document.getElementById("download-btn2")
-        .addEventListener("click", () => {
-            const canvas = this.document.getElementById("canvas");
-            console.log(canvas);
-            console.log(window);
-            var opt = {
-                margin: 1.5,
-                filename: 'atCertificate.pdf',
-                image: { type: 'jpeg', quality: 1 },
-                html2canvas: { scale: 1 },
-                jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
-            };
-            html2pdf().from(canvas).set(opt).save();
-        })
-} // Function for downloading canvas as PDF 
+// window.onload = function () {
+//     document.getElementById("download-btn2")
+//         .addEventListener("click", () => {
+//             const canvas = this.document.getElementById("canvas");
+//             console.log(canvas);
+//             console.log(window);
+//             var opt = {
+//                 margin: 1.5,
+//                 filename: 'atCertificate.pdf',
+//                 image: { type: 'jpeg', quality: 1 },
+//                 html2canvas: { scale: 1 },
+//                 jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
+//             };
+//             html2pdf().from(canvas).set(opt).save();
+//         })
+// } // Function for downloading canvas as PDF 
 
 
 var canvas = document.querySelector('canvas')
@@ -123,6 +123,15 @@ downloadBtn1.addEventListener('click', function () {
 	if (tempSelct==true){
 		downloadBtn1.href = canvas.toDataURL('image/png')
 		downloadBtn1.download = 'Certificate - ' + MonthInput.value
+	} else{
+		alert("Please Fill above form first");
+	}
+
+})
+downloadBtn2.addEventListener('click', function () { 
+	if (tempSelct==true){
+		downloadBtn2.href = canvas.toDataURL('image/png')
+		downloadBtn2.download = 'Certificate - ' + MonthInput.value
 	} else{
 		alert("Please Fill above form first");
 	}
