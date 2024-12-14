86e5277a23f2587d0a9c4ccd3e4bacaf52540c46
Vivek Bhawsar <vivekcoder18@gmail.com>
2024-12-14 09:12:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"net/http"
 
-	rice "github.com/GeertJohan/go.rice"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("AT-Tool_certificate-builder").HTTPBox()))
-	println("Listning at port : 8000") 
-	http.ListenAndServe(":8000" , router)
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./build")))
+	println("Listening at port: 8000")
+	http.ListenAndServe(":8000", router)
 }
